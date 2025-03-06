3c5a391c16a787fac9f12adb61a237dee1f935b5
Rynathub <shakirovryn@gmail.com>
2025-03-06 19:40:44 +0200

    Service for building a http request

--- CryptoApp/API/EndPoints.swift
+++ CryptoApp/API/EndPoints.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import Alamofire
+
+enum EndPoints {
+    case fetchCoins(url: String = "/v1/cryptocurrency/listings/latest")
+    
+   
+    
+
+    
+    // MARK: - Constructing an URL
+
+     var url: URL? {
+        var components = URLComponents()
+        components.scheme = Constants.scheme
+        components.host = Constants.baseURL
+        components.port = Constants.port
+        components.path = self.path
+        return components.url
+    }
+    
+     private var path: String {
+        switch self {
+        case .fetchCoins(url: let url):
+            return url
+        }
+    }
+    var method: HTTPMethod {
+            switch self {
+            case .fetchCoins:
+                return .get
+            }
+        }
+    
+     var headers:HTTPHeaders {
+        switch self {
+        case .fetchCoins:
+            return [
+                "X-CMC_PRO_API_KEY": Constants.API_KEY,
+                
+            ]
+        }
+    }
+    
+     var queryItems: [String: Any] {
+        switch self {
+        case .fetchCoins:
+            return [
+                        "limit": "150",
+                        "sort": "market_cap",
+                        "convert": "USD",
+                        "aux": "cmc_rank,max_supply,circulating_supply,total_supply"
+            ]
+        }
+        
+    }
+}
