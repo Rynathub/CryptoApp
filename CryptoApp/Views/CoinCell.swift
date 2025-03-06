76b34d078a01475290c9350c6a44b0de6aa2f794
Rynathub <shakirovryn@gmail.com>
2025-03-06 20:42:45 +0200

    Reusable Cell config

--- CryptoApp/Views/CoinCell.swift
+++ CryptoApp/Views/CoinCell.swift
@@ -6,8 +6,78 @@
 //
 
 import Foundation
+import UIKit
 
 class CoinCell: UITableViewCell {
     
     static let identifier = "CoinCell"
+    
+    private(set) var coin: Coin!
+    
+    private let coinLogo: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(systemName: "questionmark")
+        iv.tintColor = .black
+        return iv
+    }()
+    
+    private let coinName: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.text = "Error"
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI SetUp
+    func configure(with coin: Coin) {
+        self.coin = coin
+        
+        self.coinName.text = coin.name
+        
+        DispatchQueue.global().async { [weak self] in
+            if let logoURL = coin.logoURL,
+               let imageViewData = try? Data(contentsOf: logoURL),
+               let logoImage = UIImage(data: imageViewData) {
+                DispatchQueue.main.async {
+                    self?.coinLogo.image = logoImage
+                }
+            }
+                
+            
+        }
+    }
+    
+    private func setupUI() {
+        self.addSubview(coinLogo)
+        self.addSubview(coinName)
+        
+        coinLogo.translatesAutoresizingMaskIntoConstraints = false
+        coinName.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            coinLogo.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            coinLogo.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
+            coinLogo.widthAnchor.constraint(equalTo: self.heightAnchor,multiplier: 0.75),
+            coinLogo.heightAnchor.constraint(equalTo: self.heightAnchor,multiplier: 0.75),
+            
+            coinName.leadingAnchor.constraint(equalTo: coinLogo.trailingAnchor, constant: 16),
+            coinName.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+
+        ])
+
+    }
 }
