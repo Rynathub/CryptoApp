106542c50b41d4af15aefcfddaf62aea04deb51b
Rynathub <shakirovryn@gmail.com>
2025-03-06 20:44:34 +0200

    View model for ViewCryptoController

--- CryptoApp/ViewModels/ViewCryptoControllerViewModel.swift
+++ CryptoApp/ViewModels/ViewCryptoControllerViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import UIKit
+
+class ViewCryptoControllerViewModel {
+    
+    var onImageLoaded: ((UIImage?) -> Void)?
+    
+    let coin: Coin
+    
+    init(_ coin: Coin) {
+        self.coin = coin
+        self.loadImage()
+    }
+    
+    private func loadImage() {
+        DispatchQueue.global().async {
+            if let logoURL = self.coin.logoURL {
+                let imageData = try? Data(contentsOf: logoURL)
+                let logoImage = UIImage(data: imageData!)
+                self.onImageLoaded?(logoImage)
+            }
+        }
+    }
+    
+    var rankLabel: String {
+        return "Rank: \(self.coin.rank)"
+    }
+    var priceLabel: String {
+        return "Price: $\(self.coin.pricingData.USD.price) USD"
+    }
+    var marketCapLabel: String {
+        return "MarketCap: $\(self.coin.pricingData.USD.market_cap)"
+    }
+    var maxSupplyLabel: String {
+        if let maxSupply = self.coin.maxSupply {
+            return "MaxSupply: \(maxSupply)"
+        }
+        else {
+            return "123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123123\n123"
+        }
+        
+        
+    }
+}
