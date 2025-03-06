de78422342d19837ad35a8364f4b10846fa713ad
Rynathub <shakirovryn@gmail.com>
2025-03-06 20:45:38 +0200

    ViewModel for HomeController

--- CryptoApp/ViewModels/HomeControllerViewModel.swift
+++ CryptoApp/ViewModels/HomeControllerViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+class HomeControllerViewModel {
+    
+    var coinsUpdated: (() -> Void)?
+    var onErrorMessage: ((CoinServiceError) -> Void)?
+    
+    private(set) var coins: [Coin] = [] {
+        didSet {
+            self.coinsUpdated?()
+        }
+    }
+    
+    init() {
+        self.fetchCoins()
+    }
+
+    
+    func fetchCoins() {
+        let endpoint = EndPoints.fetchCoins()
+        
+        CoinService.shared.performRequest(endpoint: endpoint,type: CoinArray.self) { [weak self] result in
+            switch result {
+            case .success(let coins):
+                print("Amount of fetched coins: \(coins.data.count)")
+                self?.coins = coins.data
+            case .failure(let error):
+                guard let serviceError = error as? CoinServiceError else {
+                    
+                    let fallbackError = CoinServiceError.unknown(error.localizedDescription)
+                            self?.onErrorMessage?(fallbackError)
+                    return
+                }
+
+                self?.onErrorMessage?(serviceError)
+            }
+        }
+    }
+}
