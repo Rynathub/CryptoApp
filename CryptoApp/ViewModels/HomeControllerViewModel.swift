d3fd50a0e7099f117b703a2c02651ab6a3193150
Rynathub <shakirovryn@gmail.com>
2025-04-07 02:51:55 +0300

    Has been implemented search bar with searh logic

--- CryptoApp/ViewModels/HomeControllerViewModel.swift
+++ CryptoApp/ViewModels/HomeControllerViewModel.swift
@@ -6,17 +6,20 @@
 //
 
 import Foundation
+import UIKit
 
 class HomeControllerViewModel {
     
     var coinsUpdated: (() -> Void)?
     var onErrorMessage: ((CoinServiceError) -> Void)?
     
-    private(set) var coins: [Coin] = [] {
+    private(set) var allCoins: [Coin] = [] {
         didSet {
             self.coinsUpdated?()
         }
     }
+    
+    private(set) var filteredCoins : [Coin] = []
     
     init() {
         self.fetchCoins()
@@ -30,7 +33,7 @@ class HomeControllerViewModel {
             switch result {
             case .success(let coins):
                 print("Amount of fetched coins: \(coins.data.count)")
-                self?.coins = coins.data
+                self?.allCoins = coins.data
             case .failure(let error):
                 guard let serviceError = error as? CoinServiceError else {
                     
@@ -42,5 +45,43 @@ class HomeControllerViewModel {
                 self?.onErrorMessage?(serviceError)
             }
         }
+    }
+}
+
+extension HomeControllerViewModel {
+    func inSearchMode(_ searchController: UISearchController) -> Bool{
+        let isActive = searchController.isActive
+        let searchText = searchController.searchBar.text ?? ""
+        return isActive && !searchText.isEmpty
+    }   
+    func updateSearchController(searchBarText: String?) {
+        // If `searchBarText` is nil or empty after lowercasing, just show all coins
+        guard let searchText = searchBarText?.lowercased(), !searchText.isEmpty else {
+            self.filteredCoins = allCoins
+            self.coinsUpdated?()
+            return
+        }
+
+        // Filter
+        self.filteredCoins = allCoins.filter {
+            $0.name.lowercased().contains(searchText)
+        }
+
+        // Sort
+        self.filteredCoins = filteredCoins.sorted {
+            let lhs = $0.name.lowercased()
+            let rhs = $1.name.lowercased()
+
+            let lhsHasPrefix = lhs.hasPrefix(searchText)
+            let rhsHasPrefix = rhs.hasPrefix(searchText)
+
+            switch (lhsHasPrefix, rhsHasPrefix) {
+            case (true, false):  return true
+            case (false, true):  return false
+            default:             return lhs < rhs
+            }
+        }
+
+        self.coinsUpdated?()
     }
 }
